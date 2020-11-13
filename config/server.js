e0269ad5d65daf6ac609ad8df789b38ec88988c9
Maciej Glowacki <m.glowacki01@gmail.com>
2020-11-13 18:39:52 +0100

    Initial setup with DB

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b5ed6c5b862febd9fcfc750b4cafd665'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '50f3af39-4cb0-41ca-bd4a-3a7b852fe233'
+};
