741c83924d2a9af75f4762bc33e841bb50e62edb
Maciej Glowacki <m.glowacki01@gmail.com>
2020-12-28 14:51:23 +0100

    Email customization

--- api/email/controllers/Email.js
+++ api/email/controllers/Email.js
@@ -12,22 +12,30 @@ module.exports = {
   send: async (ctx) => {
     console.log({ custom: ctx.request.body });
     const body = ctx.request.body;
-    const sendTo = body.email;
+    const sendTo = process.env.EMAIL_ADDRESS_TO;
     strapi.log.debug(`Trying to send an email to ${sendTo}`);
+    const article = body.art
+      ? `Osoba jest zainteresowana: <b>${body.art.title}</b> - (id: ${body.art.id})`
+      : "";
 
     try {
       const emailOptions = {
-        to: sendTo,
+        from: body.email,
+        to: process.env.EMAIL_ADDRESS_TO,
         subject: body.subject,
         html: `<h1>${body.subject}</h1>
+        <br/>
         <p>Od: ${body.email}</p>
-        <p>Wiadomosć:</p>
+        <br/>
+        <p>${article}</p>
+        <br/>
         <p>${body.text}.</p>`,
       };
       await strapi.plugins["email"].services.email.send(emailOptions);
       strapi.log.debug(`Email sent to ${sendTo}`);
       ctx.send({ message: "Email sent" });
     } catch (err) {
+      console.log(err);
       strapi.log.error(`Error sending email to ${sendTo}`, err);
       ctx.send({ error: "Error sending email" });
     }
